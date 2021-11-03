1dc24173b1ccd6dcb2f6f4d6f05cc4ecaaab433d
Lútsen Stellingwerff <lutsen.stellingwerff@gmail.com>
2021-11-03 15:00:10 +0100

    Update TemplateInstallerPlugin.php

--- src/TemplateInstallerPlugin.php
+++ src/TemplateInstallerPlugin.php
@@ -14,7 +14,7 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class TemplateInstallerPlugin implements PluginInterface
+abstract class TemplateInstallerPlugin implements PluginInterface
 {
 	public function activate(Composer $composer, IOInterface $io)
 	{
