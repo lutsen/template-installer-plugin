e30de1d011439109e9bd3f3c7a794bb78a19aeee
Lútsen Stellingwerff <lutsen.stellingwerff@gmail.com>
2021-11-03 15:12:25 +0100

    Update TemplateInstallerPlugin.php
    Updated to work with Composer 2

--- src/TemplateInstallerPlugin.php
+++ src/TemplateInstallerPlugin.php
@@ -14,12 +14,22 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-abstract class TemplateInstallerPlugin implements PluginInterface
+class TemplateInstallerPlugin implements PluginInterface
 {
 	public function activate(Composer $composer, IOInterface $io)
 	{
 		$installer = new TemplateInstaller($io, $composer);
 		$composer->getInstallationManager()->addInstaller($installer);
+	}
+
+	public function deactivate(Composer $composer, IOInterface $io)
+	{
+		// Not needed
+	}
+
+	public function uninstall(Composer $composer, IOInterface $io)
+	{
+		// Not needed
 	}
 }
 
